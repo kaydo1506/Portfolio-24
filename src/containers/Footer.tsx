60aae7a58541f36544cfb068d3fa2200e50ef944
Rachael Okedo <okedoifeoma@yahoo.com>
2024-01-22 20:15:12 +0100

    Revamp Portfolio: Update projects, enhance UI/UX, and improve performance

--- src/containers/Footer.tsx
+++ src/containers/Footer.tsx
@@ -5,14 +5,8 @@ const Footer = () => {
   return (
     <footer className="max-w-lg mx-auto text-center font-mono mb-5 text-xs">
       <SocialLinks className="flex md:hidden justify-center gap-3 mb-3" />
-      <a
-        href={footerSection.link}
-        target="_blank"
-        rel="noopener noreferrer"
-        className="hover:text-amber-900 transition"
-      >
+     
         {footerSection.title}
-      </a>
     </footer>
   );
 };
